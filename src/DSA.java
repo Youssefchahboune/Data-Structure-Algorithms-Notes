d01ada791f031d4cf128233b6a7b636f3cfdf3ad
Youssefchahboune <chahbouney2000@outlook.fr>
2022-07-11 01:47:33 -0400

    cleaned the code a little.

--- src/DSA.java
+++ src/DSA.java
@@ -1,15 +1,4 @@
 public class DSA {
-    public void sortAlgorithm(int[] array){
-        for (int i = 0; i < array.length; i++){
-            for(int j = (i+1); j < array.length; j++){
-                if (array[j] < array[i]){
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
-    }
 
     public void selectionSort(int[] array){
         int i, j, minvalue,minIndex;
